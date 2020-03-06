1c6fd3392380d482828594fa2204ac0913a4d740
Christopher Couch <33438131+supergus@users.noreply.github.com>
2020-03-06 02:19:39 -0500

    howdy

--- spinup/algos/tf1/cc_exercise_simplest_policy/foo.py
+++ spinup/algos/tf1/cc_exercise_simplest_policy/foo.py
@@ -1,3 +1,13 @@
 import gym
 import liveline_gym
 env = gym.make('liveline-v0', verbosity=0)
+
+# After pasting ddpg and core.mlp_actor_critic...
+
+from spinup.utils.run_utils import setup_logger_kwargs
+logger_kwargs = setup_logger_kwargs('foo_experiment', 42)
+
+ddpg(lambda: gym.make('liveline-v0'), actor_critic=core.mlp_actor_critic,
+     ac_kwargs=dict(hidden_sizes=[256] * 2),
+     gamma=0.99, seed=42, epochs=3,
+     logger_kwargs=logger_kwargs)
