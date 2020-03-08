6c7bf2e91fb43ee74b44275d111bcbd27f08fd14
Christopher Couch <33438131+supergus@users.noreply.github.com>
2020-03-08 00:23:22 -0500

    howdy

---
+++ spinup/algos/tf1/ddpg/cc_investigate_nan_test_actions.py
@@ -0,0 +1,8 @@
+# set values for kwargs when we normally call ddpg
+# cut and paste ddpg.py script down to line 198
+# manually run a tf session
+# try to get outputs by running ops with an observation
+
+# WHOA... get_action() runs just fine, no NAN values, when doing a rollout....
+# why is it a problem when doing the test???
+
