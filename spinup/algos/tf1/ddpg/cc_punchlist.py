42b24d0734356d17757a9a05bb21c348ab327eff
Christopher Couch <33438131+supergus@users.noreply.github.com>
2020-03-09 06:27:12 -0400

    howdy

--- spinup/algos/tf1/ddpg/cc_punchlist.py
+++ spinup/algos/tf1/ddpg/cc_punchlist.py
@@ -16,5 +16,8 @@
 #  resume training. Important - with 20+ hrs of training time, you WILL lose power etc.
 #  Can we also load progress through GridSearch?
 
+# TODO: Add another metric for logging... nrmse_range versus target output values for (1) base dataset and
+#  (2) controlled output. Pre-select B random batches (~250? see model pkg) and use the same batches for
+#  (1) and (2).
 
 
